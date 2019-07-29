3a9afeb293a554afd14b3ac0bd126d756ff1c766
qkjlu <lucas_mas@msn.com>
2019-07-29 17:47:28 +0200

    sql select exemple + vpn tuto

---
+++ sql/select_assy.sql
@@ -0,0 +1,11 @@
+select *
+from [PROD_MSI$Prod_ Order Line]
+where [Prod_ Order No_] = 'OF1552982'
+
+select [Comp_ Serial No_], [Comp_ Item No_], [Comp_ Description]
+from [PROD_MSI$Traçabilité OF]
+where [Prod_ Order No_] = 'OF1552982'
+
+select *
+from [PROD_MSI$Traçabilité OF]
+where [Prod_ Order No_] = 'OF1341514'
