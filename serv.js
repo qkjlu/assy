2a5b58c06e8c48390fe54449e2d00a655bef9f3d
qkjlu <lucas_mas@msn.com>
2019-07-30 17:17:39 +0200

    cc

--- serv.js
+++ serv.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sql = require("msnodesqlv8");
+const connectionString = "server=ser-sql2011;Database=NAV_MSI_PROD;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/file/:filename', (req, res) => {
-  const fileName = req.params.filename
-  res.send(fileName)
+app.get('/of/:of', async (req, res) => {
+  console.log(req.method +" "+ req.url)
+  const query = `select [Comp_ Serial No_], [Comp_ Item No_], [Comp_ Description]
+  from [PROD_MSI$Traçabilité OF]
+  where [Prod_ Order No_] = 'OF${req.params.of}'`;
+
+  sql.query(connectionString, query, (err, rows) => {
+    if(err) {
+      res.sendStatus(500)
+      throw err
+    }
+    res.send(rows).status(200)
+  });
 })
